0b6929ac131f45d50d99a1ebe0e8225aed38ac8c
Anne Cecile Mechin <ac.mechin@gmail.com>
2025-12-03 15:30:12 +0100

    restore legal path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "admin/dashboard#index"
+  get "legal", to: "pages#legal"
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
